0573c5c9bf89c2109c1a111e8fa5ae5b3d785910
jumpei.ando <ikkyunyukon0201@yahoo.co.jp>
2018-06-18 11:34:29 +0000

    user login ver3

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,18 @@
 Rails.application.routes.draw do
-  root to: 'tasks#index'
+  get 'sessions/new'
+
+  get 'sessions/create'
+
+  get 'sessions/destroy'
+
+  root to: 'toppages#index'
+  
+  get 'login', to: 'sessions#new'
+  post 'login', to: 'sessions#create'
+  delete 'logout', to: 'sessions#destroy'
+
+  
+  get 'signup', to: 'users#new'
   resources :tasks
+  resources :users, only: [:index, :show, :new, :create]
 end
